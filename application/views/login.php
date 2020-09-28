299fcaa11b622137ed9ea66613bc1ad066e97960
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-28 11:51:33 +0530

    translate_2EEnglish

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>BSEU | Yönetim Paneli Giriş</title>
+  <title>BSEU | Admin Panel Login</title>
   <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
   <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"
@@ -23,7 +23,7 @@
     <div class="login-logo">
       <a href="#">
         <b>BSEU</b>
-        <br>Yönetim Paneli</a>
+        <br>Management panel</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
