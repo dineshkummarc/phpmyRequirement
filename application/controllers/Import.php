cbbfe7510e710c9c06b8447f2aa3f0a13a08cd7b
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-28 23:12:11 +0530

    updated

--- application/controllers/Import.php
+++ application/controllers/Import.php
@@ -1,7 +1,8 @@
 <?php if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
 
+// require APPPATH . '/third_party/vendor/autoload.php';
+require_once(APPPATH.'/config/autoload.php');
 require APPPATH . '/libraries/BaseController.php';
-require APPPATH . '/third_party/vendor/autoload.php';
 require APPPATH . '/libraries/mypdf.php';
 
 use Akeneo\Component\SpreadsheetParser\SpreadsheetParser;
