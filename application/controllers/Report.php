ac4b9203b9c12140f7fd43f4940a5cb97f1eff93
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-29 11:02:15 +0530

    updated

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1,7 +1,9 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 
-require APPPATH . '/libraries/BaseController.php';
+// require APPPATH . '/libraries/BaseController.php';
+require_once(APPPATH.'/libraries/BaseController.php');
+
 require APPPATH . '/third_party/vendor/autoload.php';
 require APPPATH . '/libraries/mypdf.php';
 
