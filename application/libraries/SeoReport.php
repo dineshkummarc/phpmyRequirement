8105d2c91e66da0b32902cdb32cf0ad9ee6b8d4f
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-09-30 22:35:01 +0530

    updated

--- application/libraries/SeoReport.php
+++ application/libraries/SeoReport.php
@@ -1,6 +1,7 @@
 <?php if (! defined ( 'BASEPATH' ))	exit ( 'No direct script access allowed' );
 
-require APPPATH . '/third_party/vendor/autoload.php';
+// require APPPATH . '/third_party/vendor/autoload.php';
+require_once(APPPATH.'/config/autoload.php');
 require APPPATH . '/libraries/mypdf.php';
 
 /**
