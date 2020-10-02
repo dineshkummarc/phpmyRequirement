562ffcf4ebefc98c2fd54c3aa2bf83e97991df50
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-02 12:00:15 +0530

    add_name

--- application/views/caching.php
+++ application/views/caching.php
@@ -22,18 +22,21 @@
 		                    <tr>
 		                      <th>Id</th>
 		                      <th>Page Name</th>
+							  <th>Requester Name</th>
 		                      <th>Page URL</th>
 		                      <th class="alignCenter">Actions</th>
 		                    </tr>
 		                    <tr>
 		                    	<td>1</td>
 		                    	<td>Login</td>
+								<td>Admin</td>
 		                    	<td>/login</td>
 		                    	<td class="alignCenter"><a href="#" id="refreshSpinner_1" data-spinner="1" data-path="login" class="delCache"><i  class="fa fa-refresh fa-fw"></i></a></td>
 		                    </tr>
 		                    <tr>
 		                    	<td>2</td>
 		                    	<td>Dashboard</td>
+								<td>Admin</td>
 		                    	<td>/dashboard</td>
 		                    	<td class="alignCenter"><a href="#" id="refreshSpinner_2" data-spinner="2" data-path="dashboard" class="delCache"><i class="fa fa-refresh fa-fw"></i></a></td>
 		                    </tr>
