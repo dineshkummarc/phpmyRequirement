64d5de3b83f247da3630fcb4c5ca676f1982ad3a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-02 11:58:37 +0530

    fullscreen

--- application/views/caching.php
+++ application/views/caching.php
@@ -8,7 +8,7 @@
     </section>
     <section class="content">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
               <!-- general form elements -->
                 <div class="box box-primary">
                     <div class="box-header">
