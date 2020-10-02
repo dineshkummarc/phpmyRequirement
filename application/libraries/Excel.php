71ed1bce016a045c43caf99eda122479f97b4b20
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-02 17:52:39 +0530

    update

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -3,9 +3,8 @@ if (! defined ( 'BASEPATH' ))
 	exit ( 'No direct script access allowed' );
 
 // require_once dirname ( __FILE__ ) . '/../third_party/PHPExcel/Classes/PHPExcel.php';
-// require_once(APPPATH.'/third_party/PHPExcel/Classes/PHPExcel.php');
-
-require_once(APPPATH.'/third_party/spreadsheet-parser/src/SpreadsheetParser.php');
+require_once(APPPATH.'/third_party/PHPExcel/Classes/PHPExcel.php');
+// require_once(APPPATH.'/third_party/spreadsheet-parser/src/SpreadsheetParser.php');
 
 class Excel extends PHPExcel {
 	public function __construct() {
