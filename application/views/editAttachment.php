006fca7013776140c108354cba0e3ff721a56f4a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-02 20:44:06 +0530

    column-width

--- application/views/editAttachment.php
+++ application/views/editAttachment.php
@@ -36,7 +36,7 @@ if(!empty($rawData))
     
         <div class="row">
             <!-- left column -->
-            <div class="col-md-8">
+            <div class="col-md-12">
               <!-- general form elements -->
                 
                 <div class="box box-primary">
