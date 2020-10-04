fd9140c5be6ca970c1ca33d516c6e5d0272fc067
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-04 10:24:32 +0530

    added

---
+++ application/controllers/Caching.php
@@ -0,0 +1,36 @@
+<?php if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
+
+// require APPPATH . '/libraries/BaseController.php';
+require_once(APPPATH.'/libraries/BaseController.php');
+
+
+class Caching extends BaseController {
+	/**
+	 * This is default constructor of the class
+	 */
+	public function __construct() {
+		parent::__construct ();
+		$this->isLoggedIn ();
+	}
+	
+	function index(){
+		if ($this->isAdmin () == TRUE) {
+			$this->loadThis ();
+		} else {
+			
+			$this->global ['pageTitle'] = 'RequirementAnalysis : Cache Management';
+			$this->loadViews("caching", $this->global, NULL, NULL);
+		}
+	}
+	
+	function deleteCache(){
+		$path = $this->input->post("path");
+		$this->output->clear_path_cache($path);
+		echo(json_encode(array("status"=>"success")));
+	}
+	
+	function deleteAllCache(){
+		$this->output->clear_all_cache();
+		echo(json_encode(array("status"=>"success")));
+	}
+}
