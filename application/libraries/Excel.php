f7b6c124a92ba77d1497199b87226908e0a34442
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-04 22:04:43 +0530

    revert_2_base_version

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -2,7 +2,10 @@
 if (! defined ( 'BASEPATH' ))
 	exit ( 'No direct script access allowed' );
 
-require_once dirname ( __FILE__ ) . '/../third_party/PHPExcel/Classes/PHPExcel.php';
+// require_once dirname ( __FILE__ ) . '/../third_party/PHPExcel/Classes/PHPExcel.php';
+require_once(APPPATH.'/third_party/PHPExcel/Classes/PHPExcel.php');
+// require_once(APPPATH.'/third_party/spreadsheet-parser/src/SpreadsheetParser.php');
+
 class Excel extends PHPExcel {
 	public function __construct() {
 		parent::__construct ();
