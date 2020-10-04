18713e4cb1376e2d3f48001f9c20608dd9949584
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-04 14:02:51 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/feedbacker/';
-$config['base_url'] = 'http://local.rocket.co.in:8081/test1/requirementanalysis2/';
+$config['base_url'] = 'http://local.rocket.co.in:8081/test1/requirementanalysis/';
 
 /*
 |--------------------------------------------------------------------------
