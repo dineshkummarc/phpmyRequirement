c4a34b86149e56e89f9395a5a9dde8ae1503dcaa
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-05 00:10:39 +0530

    update_rhs_version

--- application/views/changePassword.php
+++ application/views/changePassword.php
@@ -8,7 +8,7 @@
     </section>
     <section class="content">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-12">
               <!-- general form elements -->
                 <div class="box box-primary">
                     <div class="box-header">
@@ -18,16 +18,16 @@
                     <form role="form" action="<?php echo base_url() ?>changePassword" method="post">
                         <div class="box-body">
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="inputPassword1">Old Password</label>
                                         <input type="password" class="form-control" id="inputOldPassword" placeholder="Old password" name="oldPassword" maxlength="20" required>
                                     </div>
                                 </div>
                             </div>
-                            <hr>
+                           <!-- <hr> -->
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="inputPassword1">New Password</label>
                                         <input type="password" class="form-control" id="inputPassword1" placeholder="New password" name="newPassword" maxlength="20" required>
@@ -35,7 +35,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="inputPassword2">Confirm New Password</label>
                                         <input type="password" class="form-control" id="inputPassword2" placeholder="Confirm new password" name="cNewPassword" maxlength="20" required>
